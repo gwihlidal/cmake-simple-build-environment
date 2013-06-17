fb97913dbfc34cda76944c42bd7ae38f2198603e
stevo.bellus <stevo.bellus@gmail.com>
2013-06-17 13:21:00 +0000

    Dependencies description syntax changed

--- scripts/AddInstallTargetForContainer.cmake
+++ scripts/AddInstallTargetForContainer.cmake
@@ -1,16 +1,19 @@
 cmake_minimum_required(VERSION 2.8)
 
+include(SBE/helpers/DependenciesParser)
     
 set(INSTALL_LIBRARIES "")
 set(INSTALL_TARGETS "")
 
 include(${DEP_INFO_FILE})
 
-foreach(dep ${DEPENDENCIES})
+ParseDependencies("${DEPENDENCIES}" ownDependenciesIds)
+
+foreach(dep ${ownDependenciesIds})
     set(depName ${${dep}_Name})
     
     list(APPEND INSTALL_TARGETS ${depName})
-endforeach()
+ endforeach()
     
 # Create the Config.cmake and ConfigVersion files
 configure_file(${CMAKE_ROOT}/Modules/SBE/templates/ContainerPackageConfig.cmake.in "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" @ONLY)
