fd2b6f5d6b511fdd2496a908d89b9ae10fee949c
stevo.bellus@gmail.com <stevo.bellus@gmail.com@8009f53a-b935-95bc-d363-12c174eb3b45>
2013-06-21 10:57:00 +0000

    update and check new dependencies implemented

---
+++ trunk/scripts/helpers/SvnHelpers.cmake
@@ -0,0 +1,41 @@
+cmake_minimum_required(VERSION 2.8)
+
+find_package(Subversion QUIET)
+if(NOT Subversion_SVN_EXECUTABLE)
+    message(FATAL_ERROR "error: could not find svn.")
+endif()
+
+function(svnGetNewestSubdirectory directoryToCheck newestSubDirectory errorReason)
+    # get directory info 
+    execute_process(
+        COMMAND ${Subversion_SVN_EXECUTABLE} list --verbose "${directoryToCheck}"
+        RESULT_VARIABLE svnResult
+        ERROR_VARIABLE err
+        OUTPUT_VARIABLE out)
+        
+    if(${svnResult} GREATER 0)
+        set(${errorReason} "Could not get list for ${directoryToCheck} due to:\n${err}" PARENT_SCOPE)
+        return()
+    endif()
+    
+    # create array from lines
+    string(REPLACE "\n" ";" out "${out}")
+    
+    # get newest subrirectory (one with highest revision) 
+    set(newestSubdir "")
+    set(highestRevision 0)
+    foreach(line ${out})
+        # get revision and directory from line  
+        string(REGEX MATCH "^[^0-9]*([0-9]+) .* [A-Z][a-z]+ [0-9]+ [0-9][0-9]:[0-9][0-9] (.+)/$" tmp "${line}")
+        set(revision ${CMAKE_MATCH_1})
+        set(subDirectory "${CMAKE_MATCH_2}")
+        
+        if(NOT "" STREQUAL "${subDirectory}" AND NOT "." STREQUAL "{subDirectory}" AND ${revision} GREATER ${highestRevision})
+            set(newestSubdir ${subDirectory}) 
+        endif() 
+    endforeach()
+    
+    set(${newestSubDirectory} ${newestSubdir} PARENT_SCOPE)
+endfunction()  
+
+       
