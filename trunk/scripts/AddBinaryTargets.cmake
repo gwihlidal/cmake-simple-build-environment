f112259de405558b4abff2f3c3360156d80c570b
stevo.bellus@gmail.com <stevo.bellus@gmail.com@8009f53a-b935-95bc-d363-12c174eb3b45>
2013-06-28 12:50:17 +0000

    Supports DECLSPEC handling.

--- trunk/scripts/AddBinaryTargets.cmake
+++ trunk/scripts/AddBinaryTargets.cmake
@@ -1,70 +1,171 @@
-cmake_minimum_required(VERSION 2.8)
-
-if(CMAKE_TOOLCHAIN_FILE)
-endif()
-
-# set AddBinaryTargets script usage flag, for checking dependencies between scripts
-set(AddBinaryTargetsIncluded "yes")
-
-set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
-set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
-set (CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib) 
-
-if(("${SOURCE_FILES}" STREQUAL "") AND ("${SOURCE_FILES_OUTOF_TEST}" STREQUAL ""))
+if(isAddBinaryTargetsIncluded)
     return()
 endif()
 
-set(sourceObjects "")
+set(isAddBinaryTargetsIncluded yes)
 
-if(NOT "${SOURCE_FILES}" STREQUAL "")
-    add_library(${PROJECT_NAME}SourceObjects OBJECT ${SOURCE_FILES})
-endif()    
+include(SBE/helpers/AddDependenciesToTarget)
 
-get_property(isSharedLibSupported GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS)
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
+set (CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
 
-if("${TYPE}" STREQUAL "Library")
-    if(${isSharedLibSupported})
+include_directories(${CMAKE_SOURCE_DIR}/src)
+
+function(addBinaryTarget)
+    CMAKE_PARSE_ARGUMENTS(bt "HANDLE_DECLSPEC;STATIC_LIBRARY" "TARGET_NAME;TARGET_TYPE;OBJECT_LIBRARY" "SOURCES;DEPENDENCY_TYPES_TO_LINK;PUBLIC_HEADERS" ${ARGN})
     
-        add_library(${PROJECT_NAME} SHARED ${SOURCE_FILES_OUTOF_TEST} $<TARGET_OBJECTS:${PROJECT_NAME}SourceObjects>)
+    if(
+        (NOT DEFINED bt_TARGET_NAME) OR
+        (NOT DEFINED bt_TARGET_TYPE) OR
+        ((NOT DEFINED bt_OBJECT_LIBRARY) AND (NOT DEFINED bt_SOURCES))
+      )
+        return()
+    endif()
+    
+    if(NOT DEFINED bt_DEPENDENCY_TYPES_TO_LINK)
+        if("Test Executable" STREQUAL "${bt_TARGET_TYPE}")
+            set(bt_DEPENDENCY_TYPES_TO_LINK "Library" "Project" "Unit Test Framework")
+        else()
+            set(bt_DEPENDENCY_TYPES_TO_LINK "Library" "Project")
+        endif()
+    endif()  
+    
+    if(DEFINED bt_OBJECT_LIBRARY)
+        set(precompilatedObjects "\$<TARGET_OBJECTS:${bt_OBJECT_LIBRARY}>")
+    endif()
+    
+    if("${bt_TARGET_TYPE}" STREQUAL "Library")
+    
+        _addLibraryTarget(
+            NAME ${bt_TARGET_NAME}
+            SOURCES ${bt_SOURCES}
+            OBJECTS ${precompilatedObjects} 
+            PUBLIC_HEADERS ${bt_PUBLIC_HEADERS}
+            DEPENDENCY_TYPES_TO_LINK ${bt_DEPENDENCY_TYPES_TO_LINK}
+            HANDLE_DECLSPEC ${bt_HANDLE_DECLSPEC}
+            STATIC_LIBRARY ${bt_STATIC_LIBRARY})
+            
+    elseif("${bt_TARGET_TYPE}" STREQUAL "Executable")
+    
+        _addExecutable(
+            NAME ${bt_TARGET_NAME}
+            SOURCES ${bt_SOURCES}
+            OBJECTS ${precompilatedObjects} 
+            DEPENDENCY_TYPES_TO_LINK ${bt_DEPENDENCY_TYPES_TO_LINK})
+            
+    elseif("${bt_TARGET_TYPE}" STREQUAL "Test Executable")
+    
+        _addExecutable(
+            NAME ${bt_TARGET_NAME}
+            SOURCES ${bt_SOURCES}
+            OBJECTS ${precompilatedObjects} 
+            DEPENDENCY_TYPES_TO_LINK ${bt_DEPENDENCY_TYPES_TO_LINK}
+            TEST)
+            
+    elseif("${bt_TARGET_TYPE}" STREQUAL "Object Library")
+    
+        _addObjectLibrary(
+            NAME ${bt_TARGET_NAME}
+            SOURCES ${bt_SOURCES}
+            HANDLE_DECLSPEC ${bt_HANDLE_DECLSPEC})
+    endif()
+endfunction()
+
+function(_addLibraryTarget)
+    CMAKE_PARSE_ARGUMENTS(lib "" "NAME;HANDLE_DECLSPEC;STATIC_LIBRARY" "SOURCES;OBJECTS;PUBLIC_HEADERS;DEPENDENCY_TYPES_TO_LINK" ${ARGN})
+    
+    get_property(isSharedLibSupported GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS)
+    
+    if(isSharedLibSupported AND NOT lib_STATIC_LIBRARY)
+        add_library(${lib_NAME} SHARED ${lib_SOURCES} ${lib_OBJECTS})
         
-        set_target_properties(${PROJECT_NAME}
+        set_target_properties(${lib_NAME}
     	    PROPERTIES
     		    # create *nix style library versions + symbolic links
     		    VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
     		    SOVERSION ${VERSION_MAJOR}.${VERSION_MINOR}
     		    INSTALL_RPATH ".")
-    		    
-        if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "MSVC")
-            set(IES "__declspec(dllexport)")
-            configure_file(${CMAKE_ROOT}/Modules/SBE/templates/ImportExportSpecifier.h.in "${PROJECT_BINARY_DIR}/GeneratedSources/ImportExportSpecifier.h" @ONLY)
-        else()
-            set(IES "")
-            configure_file(${CMAKE_ROOT}/Modules/SBE/templates/ImportExportSpecifier.h.in "${PROJECT_BINARY_DIR}/GeneratedSources/ImportExportSpecifier.h" @ONLY)            
-        endif()
-        
-        include_directories(${PROJECT_BINARY_DIR}/GeneratedSources)
     else()
-        add_library(${PROJECT_NAME} STATIC ${SOURCE_FILES_OUTOF_TEST} $<TARGET_OBJECTS:${PROJECT_NAME}SourceObjects>)
-	endif()
-	
-    set(INSTALL_LIBRARIES ${PROJECT_NAME})	    
-elseif("${TYPE}" STREQUAL "Executable")
+        add_library(${lib_NAME} STATIC ${lib_SOURCES} ${lib_OBJECTS})            
+    endif()
+    
+    addDependencies(
+        TARGET ${lib_NAME} 
+        DEPENDENCY_TYPES ${lib_DEPENDENCY_TYPES_TO_LINK} 
+        CONTAIN_DECLSPEC_FLAG containsDependnecyDeclspec)
 
-    add_executable(${PROJECT_NAME} ${SOURCE_FILES_OUTOF_TEST} $<TARGET_OBJECTS:${PROJECT_NAME}SourceObjects>)
+    if(lib_HANDLE_DECLSPEC OR containsDependnecyDeclspec)
+        _handleDeclSpec(${lib_NAME})
+    endif()
+            
+    if(DEFINED lib_PUBLIC_HEADERS)
+        set_property(TARGET ${lib_NAME} PROPERTY SBE_PUBLIC_HEADERS ${lib_PUBLIC_HEADERS})
+    endif()
+    
+    set_property(TARGET ${lib_NAME} PROPERTY SBE_TYPE "Library")
+endfunction()
 
-    if(${isSharedLibSupported})
-        set_target_properties(${PROJECT_NAME}
+function(_addExecutable)
+    CMAKE_PARSE_ARGUMENTS(exe "TEST" "NAME" "SOURCES;OBJECTS;DEPENDENCY_TYPES_TO_LINK" ${ARGN})
+    
+    if(exe_TEST)
+        configure_file(${CMAKE_ROOT}/Modules/SBE/templates/CppUTestRunAllTests.cpp.in "${PROJECT_BINARY_DIR}/GeneratedSources/RunAllTests.cpp" @ONLY)
+
+        add_executable(${exe_NAME} ${PROJECT_BINARY_DIR}/GeneratedSources/RunAllTests.cpp ${exe_SOURCES} ${exe_OBJECTS})
+    
+        set_property(TARGET ${exe_NAME} PROPERTY SBE_IS_TEST_TARGET "yes")
+        set_property(TARGET ${exe_NAME} PROPERTY SBE_TYPE "Test Executable")
+    else()
+        add_executable(${exe_NAME} ${exe_SOURCES} ${exe_OBJECTS})
+        set_property(TARGET ${exe_NAME} PROPERTY SBE_TYPE "Executable")
+    endif()
+
+    get_property(isSharedLibSupported GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS)
+        
+    if(isSharedLibSupported)
+        set_target_properties(${exe_NAME}
     	    PROPERTIES
     		    INSTALL_RPATH "../lib")
     endif()
-    set(INSTALL_EXECUTABLE ${PROJECT_NAME})
-else()
-    return()    
-endif()
+    
+    addDependencies(
+        TARGET ${exe_NAME} 
+        DEPENDENCY_TYPES ${exe_DEPENDENCY_TYPES_TO_LINK} 
+        CONTAIN_DECLSPEC_FLAG containsDependnecyDeclspec)
+    
+    if(containsDependnecyDeclspec)
+        _handleDeclSpec(${exe_NAME})
+    endif()
+endfunction()
 
-include_directories(${CMAKE_SOURCE_DIR}/src)
+function(_addObjectLibrary)
+    CMAKE_PARSE_ARGUMENTS(obj "" "NAME;HANDLE_DECLSPEC" "SOURCES" ${ARGN})
+    
+    add_library(${obj_NAME} OBJECT ${obj_SOURCES})
+    
+    doesDependenciesContainsDeclSpecs(dependenciesContainsDeclspecs)
+    
+    if(obj_HANDLE_DECLSPEC OR dependenciesContainsDeclspecs)
+        _handleDeclSpec(${obj_NAME})
+    endif()
+endfunction()
 
-set(DEP_TARGET ${PROJECT_NAME})
-set(DEP_TYPES_TO_ADD "Library" "Project")
-include(SBE/helpers/AddDependenciesToTarget)
+function(_handleDeclSpec targetName)
+    get_target_property(compilationFlags ${targetName} COMPILE_FLAGS)
+    
+    if("compilationFlags-NOTFOUND" STREQUAL "${compilationFlags}")
+        set(compilationFlags "")
+    endif()
+    
+    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "MSVC")
+        set(defs "${compilationFlags} -D__EXPORT=__declspec(dllexport) -D__IMPORT=__declspec(dllimport)")
+    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
+        set(defs "${compilationFlags} -D__EXPORT= -D__IMPORT=")
+    else()
+        set(defs "${compilationFlags} -D__EXPORT= -D__IMPORT=")
+    endif()
 
+    set_target_properties(${targetName} PROPERTIES COMPILE_FLAGS ${defs})
+    set_property(TARGET ${targetName} PROPERTY SBE_CONTAINS_DECLSPEC yes)
+endfunction()
--- trunk/scripts/AddTestTargets.cmake
+++ trunk/scripts/AddTestTargets.cmake
@@ -1,33 +1,15 @@
 cmake_minimum_required(VERSION 2.8)
 
-# check if this script is after AddBinaryTargets
-if (NOT ${AddBinaryTargetsIncluded})
-    message(FATAL_ERROR "AddTestTargets scripts has to be included after AddBinaryTargets.")
-endif()
 
-configure_file(${CMAKE_ROOT}/Modules/SBE/templates/CppUTestRunAllTests.cpp.in "${PROJECT_BINARY_DIR}/GeneratedSources/RunAllTests.cpp" @ONLY)
-
-add_executable(${PROJECT_NAME}Test ${PROJECT_BINARY_DIR}/GeneratedSources/RunAllTests.cpp ${TEST_FILES} $<TARGET_OBJECTS:${PROJECT_NAME}SourceObjects>)
-
-if(${isSharedLibSupported})
-    set_target_properties(${PROJECT_NAME}Test
-        PROPERTIES
-    	    INSTALL_RPATH "../lib")
-endif()    	    
-	    
-set(INSTALL_TEST_EXECUTABLE ${PROJECT_NAME}Test)
-
-include_directories(${CMAKE_SOURCE_DIR}/src)
-
-set(DEP_TARGET ${PROJECT_NAME}Test)
-set(DEP_TYPES_TO_ADD "Library" "Project" "Unit Test Framework")
-include(SBE/helpers/AddDependenciesToTarget)
-
-if(${CMAKE_CROSSCOMPILING})
-    add_custom_target(test COMMENT "Not possible to run test because of cross-compiling." DEPENDS ${PROJECT_NAME}Test)
-else()
-    add_custom_target(test
-        COMMAND cmake -E remove -f cpputest_*.xml 
-        COMMAND bin/${PROJECT_NAME}Test \${CPPUTEST_FLAGS}
-        DEPENDS ${PROJECT_NAME}Test)
-endif()    
+function(addTestTarget)
+    CMAKE_PARSE_ARGUMENTS(test "" "EXECUTABLE_NAME" "" ${ARGN})
+    
+    if(${CMAKE_CROSSCOMPILING})
+        add_custom_target(test COMMENT "Not possible to run test because of cross-compiling." DEPENDS ${test_EXECUTABLE_NAME})
+    else()
+        add_custom_target(test
+            COMMAND cmake -E remove -f cpputest_*.xml 
+            COMMAND bin/${test_EXECUTABLE_NAME} \${CPPUTEST_FLAGS}
+            DEPENDS ${test_EXECUTABLE_NAME})
+    endif()
+endfunction()        
