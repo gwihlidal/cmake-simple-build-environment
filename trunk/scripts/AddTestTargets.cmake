4c6d73b194a855e85940ac3b7667660992fb2336
stevo.bellus@gmail.com <stevo.bellus@gmail.com@8009f53a-b935-95bc-d363-12c174eb3b45>
2013-06-28 13:58:38 +0000

    fix for windows

--- trunk/scripts/AddTestTargets.cmake
+++ trunk/scripts/AddTestTargets.cmake
@@ -7,9 +7,15 @@ function(addTestTarget)
     if(${CMAKE_CROSSCOMPILING})
         add_custom_target(test COMMENT "Not possible to run test because of cross-compiling." DEPENDS ${test_EXECUTABLE_NAME})
     else()
+        set(testOptions "")
+        if("Windows" STREQUAL "${CMAKE_SYSTEM_NAME}")
+            set(testOptions "%CPPUTEST_FLAGS%")
+        elseif("Linux" STREQUAL "${CMAKE_SYSTEM_NAME}")
+            set(testOptions "\\\${CPPUTEST_FLAGS}")
+        endif()
         add_custom_target(test
             COMMAND cmake -E remove -f cpputest_*.xml 
-            COMMAND bin/${test_EXECUTABLE_NAME} \$CPPUTEST_FLAGS
+            COMMAND bin/${test_EXECUTABLE_NAME} ${testOptions}
             DEPENDS ${test_EXECUTABLE_NAME})
     endif()
 endfunction()        
