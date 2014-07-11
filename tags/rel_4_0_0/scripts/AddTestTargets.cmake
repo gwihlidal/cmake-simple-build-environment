0858ebccd578835578a33b31a2302ca8db53a9d6
stevo.bellus@gmail.com <stevo.bellus@gmail.com@8009f53a-b935-95bc-d363-12c174eb3b45>
2014-07-11 09:07:50 +0000

    Tagging before integrating dependencies build in build tree

---
+++ tags/rel_4_0_0/scripts/AddTestTargets.cmake
@@ -0,0 +1,30 @@
+cmake_minimum_required(VERSION 2.8)
+
+
+function(addTestTarget)
+    CMAKE_PARSE_ARGUMENTS(test "" "Executable" "" ${ARGN})
+    
+    if(${CMAKE_CROSSCOMPILING})
+        add_custom_target(test COMMENT "Not possible to run test because of cross-compiling." DEPENDS ${test_Executable})
+    else()
+        set(testOptions "")
+        if("Windows" STREQUAL "${CMAKE_SYSTEM_NAME}")
+            set(testOptions "\$(CPPUTEST_FLAGS)")
+            
+            add_custom_target(test
+                COMMAND set "PATH=${CMAKE_CURRENT_BINARY_DIR}/dependencies/installation/bin;%%PATH%%" 
+                COMMAND cmake -E remove -f cpputest_*.xml 
+                COMMAND bin/${test_Executable} ${testOptions}
+                DEPENDS ${test_Executable})
+        elseif("Linux" STREQUAL "${CMAKE_SYSTEM_NAME}")
+            set(testOptions "\${CPPUTEST_FLAGS}")
+            
+            add_custom_target(test
+                COMMAND cmake -E remove -f cpputest_*.xml 
+                COMMAND bin/${test_Executable} ${testOptions}
+                DEPENDS ${test_Executable})
+        endif()
+        
+
+    endif()
+endfunction()        
---
+++ tags/rel_4_0_0/scripts/DeployDependencies.cmake
@@ -0,0 +1,50 @@
+cmake_minimum_required(VERSION 2.8)
+
+if(NOT DEP_SRC_DEPLOYMENT_PATH)
+    set(DEP_SOURCE_DIR ${PROJECT_SOURCE_DIR})
+    
+    include(SBE/ExportDependencies)
+else()
+    set(DEP_SOURCES_PATH "${DEP_SRC_DEPLOYMENT_PATH}/sources")
+    set(DEP_INFO_FILE "${DEP_SRC_DEPLOYMENT_PATH}/info/info.cmake")    
+endif()
+    
+include(SBE/InstallDependencies)
+    
+include(${DEP_INFO_FILE} OPTIONAL)
+include(${DEP_INST_INFO_FILE} OPTIONAL)
+
+# load variables of all dependencies
+include(SBE/helpers/DependenciesParser)
+
+GetOverallDependencies("${DEPENDENCIES}" MyOverallDependencies)
+
+foreach(dep ${MyOverallDependencies})
+    set(depName ${${dep}_Name})
+
+    list(APPEND depNames ${depName})
+endforeach()
+
+unset(MyOverallDependencies)
+
+if(DEFINED depNames)
+    foreach(depName ${depNames})
+        find_package(${depName} REQUIRED CONFIG PATHS ${DEP_INSTALL_PATH}/config NO_DEFAULT_PATH)
+    endforeach()
+    
+    unset(depNames)
+endif()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
