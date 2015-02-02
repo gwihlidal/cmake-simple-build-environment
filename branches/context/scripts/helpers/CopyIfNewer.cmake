9e54196522ff9920431402e76fc681ba49c4a10b
stevo.bellus@gmail.com <stevo.bellus@gmail.com@8009f53a-b935-95bc-d363-12c174eb3b45>
2015-02-02 11:41:47 +0000

    Fixing imported VBTs

--- branches/context/scripts/helpers/CopyIfNewer.cmake
+++ branches/context/scripts/helpers/CopyIfNewer.cmake
@@ -6,7 +6,22 @@ if(NOT DEFINED DESTINATION)
     message(SEND_ERROR "DESTINATION has to be defined")
 endif()
 
-if(${SOURCE} IS_NEWER_THAN ${DESTINATION})
+if(IS_DIRECTORY ${SOURCE})
+    if(DEFINED MESSAGE)
+        message(${MESSAGE})
+    endif()
+    
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${SOURCE} ${DESTINATION}
+        RESULT_VARIABLE result)
+    if(NOT 0 EQUAL ${result})
+        message(FATAL_ERROR "Could not copy ${SOURCE} to ${DESTINATION}")
+    endif()
+    if(DEFINED TIMESTAMP_FILE)
+        execute_process(
+        COMMAND ${CMAKE_COMMAND} -E touch ${TIMESTAMP_FILE})
+    endif()
+elseif(${SOURCE} IS_NEWER_THAN ${DESTINATION})
     if(DEFINED MESSAGE)
         message(${MESSAGE})
     endif()
@@ -15,7 +30,7 @@ if(${SOURCE} IS_NEWER_THAN ${DESTINATION})
         COMMAND ${CMAKE_COMMAND} -E copy ${SOURCE} ${DESTINATION}
         RESULT_VARIABLE result)
     if(NOT 0 EQUAL ${result})
-        message(FATAL_ERROR "")
+        message(FATAL_ERROR "Could not copy ${SOURCE} to ${DESTINATION}")
     endif()
     if(DEFINED TIMESTAMP_FILE)
         execute_process(
