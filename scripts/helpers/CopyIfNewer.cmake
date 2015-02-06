66d7cfc00482941807cfc70dc0545a7c3800a4b0
stevo.bellus <stevo.bellus@gmail.com>
2015-02-06 12:42:18 +0000

    Fix

--- scripts/helpers/CopyIfNewer.cmake
+++ scripts/helpers/CopyIfNewer.cmake
@@ -7,16 +7,41 @@ if(NOT DEFINED DESTINATION)
 endif()
 
 if(IS_DIRECTORY ${SOURCE})
-    if(DEFINED MESSAGE)
-        message(${MESSAGE})
+    message("Checking DIRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+    set(isMessagePrinted no)
+    if(NOT DEFINED MESSAGE)
+        set(isMessagePrinted yes)
     endif()
     
-    execute_process(
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${SOURCE} ${DESTINATION}
-        RESULT_VARIABLE result)
-    if(NOT 0 EQUAL ${result})
-        message(FATAL_ERROR "Could not copy ${SOURCE} to ${DESTINATION}")
+    file(GLOB_RECURSE sourceFiles RELATIVE ${SOURCE} ${SOURCE}/*)    
+    file(GLOB_RECURSE destinationFiles RELATIVE ${DESTINATION} ${DESTINATION}/*)
+    # remove destination files that are missing in source
+    if(NOT "" STREQUAL "${destinationFiles}")
+        if(NOT "" STREQUAL "${sourceFiles}")
+            list(REMOVE_ITEM destinationFiles ${sourceFiles}) 
+        endif()
+        foreach(fr ${destinationFiles})
+            if(NOT isMessagePrinted)       
+                message(${MESSAGE})
+            endif()
+            file(REMOVE ${DESTINATION}/${fr})
+        endforeach()
     endif()
+    # copy source file only if is newer
+    foreach(src ${sourceFiles})
+        if(${SOURCE}/${src} IS_NEWER_THAN ${DESTINATION}/${src})
+            if(NOT isMessagePrinted)       
+                message(${MESSAGE})
+            endif()
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E copy ${SOURCE}/${src} ${DESTINATION}/${src}
+                RESULT_VARIABLE result)
+            if(NOT 0 EQUAL ${result})
+                message(FATAL_ERROR "Could not copy ${SOURCE}/${src} to ${DESTINATION}/${src}")
+            endif()
+        endif()
+    endforeach()
+    
     if(DEFINED TIMESTAMP_FILE)
         execute_process(
         COMMAND ${CMAKE_COMMAND} -E touch ${TIMESTAMP_FILE})
